089d4996acc6ee4e0be1ecd39de2a05b4afffa45
alishachang2 <changalishac@gmail.com>
2025-11-24 21:36:27 -0800

    added notes and general layout for app.py, and removed file that was not needed

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import os
-from fixed_parse import extract_pdf
+from backend.extract_parse import extract_pdf
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,23 +9,6 @@ CORS(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-@app.route("/file", methods=["POST"])
-def parse():
-    if "file" not in request.files:
-        return jsonify({"error": "No file uploaded"}), 400
-    
-    file = request.files["file"]
-
-    saved_path = "uploaded.pdf"
-    file.save(saved_path)
-
-    try:
-        result = extract_pdf(saved_path)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
